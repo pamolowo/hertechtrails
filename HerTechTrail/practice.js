140570a09e0cdce64461af5cbe2ec0b8fac322b3
pamolowo <88985213+pamolowo@users.noreply.github.com>
2023-03-30 15:56:47 +0100

    Add files via upload

---
+++ HerTechTrail/practice.js
@@ -0,0 +1,58 @@
+const pic_1 = document.getElementById('img1');
+const pic_2 = document.getElementById('img2');
+const pic_3 = document.getElementById('img3');
+const pic_4 = document.getElementById('img4');
+const pic_5 = document.getElementById('img5');
+const pic_6 = document.getElementById('img6');
+const pic_7 = document.getElementById('img7');
+const pic_8 = document.getElementById('img8');
+
+
+pic_1.addEventListener('click', (event)=>{
+    event.target.style.background= 'rebeccapurple';
+    event.target.style.color= 'white';
+  
+})
+
+pic_2.addEventListener('click', (event)=>{
+    event.target.style.background= 'blue';
+    event.target.style.color= 'white';
+  
+})
+
+pic_3.addEventListener('click', (event)=>{
+    event.target.style.background= 'crimson';
+    event.target.style.color= 'white';
+  
+})
+
+pic_4.addEventListener('click', (event)=>{
+    event.target.style.background= 'green';
+    event.target.style.color= 'white';
+  
+})
+
+pic_5.addEventListener('click', (event)=>{
+    event.target.style.background= 'deeppink';
+    event.target.style.color= 'white';
+  
+})
+
+pic_6.addEventListener('click', (event)=>{
+    event.target.style.background= 'tomato';
+    event.target.style.color= 'white';
+  
+})
+
+pic_7.addEventListener('click', (event)=>{
+    event.target.style.background= 'teal';
+    event.target.style.color= 'white';
+  
+})
+
+pic_8.addEventListener('click', (event)=>{
+    event.target.style.background= 'yellow';
+    event.target.style.color= 'white';
+  
+})
+
